08943c6d7151acc238245b3d6648087761df8723
pynickle <2330458484@qq.com>
2019-12-31 19:35:33 +0800

    test

--- main.py
+++ main.py
@@ -17,8 +17,7 @@ class GPushM:
             print("Tag Not Added!")
 
 def gaddc(message):
-    print(__file__)
-    print(os.path.dirname(os.path.abspath(__file__)))
+    print(os.path.dirname(__file__))
     git = repo.git
     git.add(".")
     git.commit("-m", message)
