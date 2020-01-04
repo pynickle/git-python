e63494e2f555501df736595b33f3d754dca38e66
pynickle <2330458484@qq.com>
2020-01-04 12:10:33 +0800

    fix

--- README.rst
+++ README.rst
@@ -8,4 +8,12 @@ Usage
 
 1. add and commit
 
-  gp addc -m "message"
+  gp addc -m <message>
+
+2. push as tag
+
+  gp pushc addt -t <tag>
+
+  gp pushc addr -t <tag> -n <name> -r <remote>
+
+  gp pushc pusht -t <tag>
--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
+import os
 from setuptools import setup, find_packages
-from setuptools.command.install import install                                      
-
-class CustomInstall(install):                                                       
-    def run(self):                                                                  
-        install.run(self)                                                     
         
+
 setup(
     name="git-python",
-    version="1.0.3",
+    version="1.1.1",
     description="combination and simplification of some useful git commands",
+    long_description = open("README.rst").read(),
     author='code-nick-python',
     author_email='2330458484@qq.com',
     url="https://github.com/pynickle/git-python",
     packages=find_packages(),
     platforms="any",
     py_modules=['main'],
+    data_files=['config.json'],
     install_requires=[
         'gitpython'
     ],
